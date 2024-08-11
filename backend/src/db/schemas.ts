ddd7802d51fc922b9dcbef7286a93ac16e2cb09f
luishbarros <luishrbr@outlook.com>
2024-08-11 19:53:27 -0300

    feat: drizzle configuration
    "Setting the drizzle configs and defining the schemas of the database"

--- backend/src/db/schemas.ts
+++ backend/src/db/schemas.ts
@@ -7,12 +7,14 @@ import {
 	timestamp
 } from "drizzle-orm/pg-core";
 
+// Definição da tabela 'user'
 export const user = pgTable("user", {
 	id: text("id").notNull().primaryKey(),
 	session_id: text("session_id").notNull(),
 	created_at: timestamp("created_at").notNull()
 });
 
+// Definição da tabela 'note'
 export const note = pgTable("note", {
 	id: text("id").notNull().primaryKey(),
 	title: text("title").notNull(),
@@ -21,14 +23,16 @@ export const note = pgTable("note", {
 	color: integer("color").notNull(),
 	file: text("file"),
 	created_at: timestamp("created_at").notNull(),
-	user_id: text("user_id").references(() => user.id)
+	user_id: text("user_id")
+		.references(() => user.id)
+		.notNull() // Chave estrangeira referenciando 'user'
 });
 
 // Definindo as relações
 export const userRelations = relations(user, ({ many }) => ({
-	notes: many(note)
+	notes: many(note) // Um 'user' tem muitas 'notes'
 }));
 
 export const noteRelations = relations(note, ({ one }) => ({
-	user: one(user)
+	user: one(user) // Uma 'note' pertence a um 'user'
 }));
